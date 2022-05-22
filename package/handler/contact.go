48e7baaeefc1a58ae33fff180aa7de26692bbf83
mrboburs <babdusalom72@gmail.com>
2022-05-22 22:03:34 +0500

    auth removed from Gets

--- package/handler/contact.go
+++ package/handler/contact.go
@@ -47,7 +47,7 @@ func (handler *Handler) CreateContactPost(ctx *gin.Context) {
 // @Failure 400,404 {object} errorResponse
 // @Failure 500 {object} errorResponse
 // @Failure default {object} errorResponse
-// @Router /auth/contacts/get [GET]
+// @Router /api/contacts/get [GET]
 //@Security ApiKeyAuth
 func (handler *Handler) GetAllContact(ctx *gin.Context) {
 	logrus := handler.logrus
