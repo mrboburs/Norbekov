4aab3e4eacb6eaab89eef318b0b24020109087b5
mrboburs <babdusalom72@gmail.com>
2022-05-23 23:15:57 +0500

    heroku2

--- command/main.go
+++ command/main.go
@@ -24,7 +24,7 @@ import (
 // @title Norbekov API
 // @version 1.0
 // @description API Server for Norbekov App
-// @host http://norbekov.herokuapp.com/swagger/index.html
+// @host norbekov.herokuapp.com/swagger/index.html
 // @BasePath /
 // @securityDefinitions.apikey ApiKeyAuth
 // @in header
