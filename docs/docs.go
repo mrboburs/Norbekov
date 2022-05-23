47bddca70b2c722c87dac22f63d34de16c9b349f
mrboburs <babdusalom72@gmail.com>
2022-05-23 23:03:33 +0500

    heroku2

--- docs/docs.go
+++ docs/docs.go
@@ -2201,7 +2201,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "http://norbekov.herokuapp.com/swagger/index.html#/Admin/create-admin_post",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Norbekov API",
